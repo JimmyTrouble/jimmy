2aeb7f95be13f7f5c301d3f86b48a92a97c27e5a
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2022-12-26 22:27:33 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("Hello, World!");
+// Specify the URL of the website you want to scrape
+const url = "https://simsimi.com/";
+
+// Send an HTTP request to the website and retrieve the HTML content
+fetch(url)
+  .then(response => response.text())
+  .then(html => {
+    // Parse the HTML content using the DOMParser
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    // Select all of the paragraphs in the HTML
+    const paragraphs = doc.querySelectorAll("p");
+
+    // Iterate over the paragraphs
+    for (const p of paragraphs) {
+      // Get the text content of the current paragraph
+      const text = p.textContent;
+
+      // Use the includes function to search for the word "message" in the text (case-insensitive)
+      if (text.toLowerCase().includes("message")) {
+        // If the word is found, log the message to the console
+        console.log(text);
+      }
+    }
+  });
+
